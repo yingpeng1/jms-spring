2e261e703d0c8a05335f4218f23a39a1efc50b22
彭颖 <1653497380@qq.com>
2018-05-30 12:30:30 +0800

    jms消息中间件

---
+++ src/main/java/com/imooc/jms/producer/AppProducer.java
@@ -0,0 +1,14 @@
+package com.imooc.jms.producer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppProducer {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("producer.xml");
+        ProducerService service = context.getBean(ProducerService.class);
+        for(int i = 0;i < 100;i++){
+            service.sendMessage("test" + i);
+        }
+        context.close();
+    }
+}
